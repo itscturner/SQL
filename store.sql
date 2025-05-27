1493b403f69b4c5f751770ae0c7cb7106551d860
Casey Turner <7030crt@gmail.com>
2025-05-27 08:29:59 -0600

    Update store.sql

--- store.sql
+++ store.sql
@@ -1,27 +1,41 @@
--- Create Store Database
+-- 1. Create Store Database
 CREATE DATABASE store;
 
--- Use Store Database
+-- 2. Use Store Database
 USE store;
 
--- Create Customers Table
+-- 3. Create Customers Table
 CREATE TABLE customers (
   customer_id INT PRIMARY KEY,
   first_name VARCHAR(50),
-  last_name VARCHAR(50),
+  last_name VARCHAR(50) NOT NULL,
   email VARCHAR(100) UNIQUE,
   phone VARCHAR(15),
   address VARCHAR(255)
   );
 
--- Show Customers Table
+-- 4. Show Customers Table
 DESCRIBE customers;
 
--- Add Customer To Customers Table
+-- 5. Add Customer To Customers Table
 INSERT INTO customers (customer_id, first_name, last_name, email, phone, address)
 VALUES
   (1, "John", "Smith", "jsmith@email.com", "123-456-7890", "123 Main Street"),
   (2, "Adam", "Price", "adamprice@email.com", "908-765-4321", "908 Main Street");
 
--- View Customers Table
+-- 6. View Customers Table
 SELECT * FROM customers;
+
+-- 7. Update Row
+UPDATE customers
+SET last_name = 'Davis', email = 'adam.davis@email.com'
+WHERE customer_id = 2;
+
+-- 8. Delete Row
+DELETE FROM customers
+WHERE customer_id = 1;
+
+-- 9. Query First Name, Last Name and Email, and Order By Last Name
+SELECT customers.first_name, customers.last_name, customers.email
+FROM customers
+ORDER BY last_name;
